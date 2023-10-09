d029f56f62d746ea16cce6cf3c3017675fdd70be
Rafal Galka <rgalka@coldrun.com>
2023-10-09 17:10:55 +0200

    chore: migrate to github docker registry

--- docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t qa-api-mock .
-docker tag qa-api-mock:latest 746295018053.dkr.ecr.eu-central-1.amazonaws.com/qa-api-mock:latest
-docker push 746295018053.dkr.ecr.eu-central-1.amazonaws.com/qa-api-mock:latest
